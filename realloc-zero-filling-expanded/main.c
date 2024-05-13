a44ecd88c0eb513a835f18bc612ad37fdb5f3634
RicardasSim <universe.nomad@gmail.com>
2024-05-13 16:36:08 +0300

    int

--- realloc-zero-filling-expanded/main.c
+++ realloc-zero-filling-expanded/main.c
@@ -3,10 +3,19 @@
 #include <stdbool.h>
 #include <string.h>
 
-// TODO: int buffer
-
 void FillBufferWithOnes( char *p_buff,
                          size_t n )
+{
+
+    for ( unsigned int i = 0; i < n; ++i )
+    {
+        p_buff[i] = 1;
+    }
+
+}
+
+void FillBufferWithOnesInt( int *p_buff,
+                            size_t n )
 {
 
     for ( unsigned int i = 0; i < n; ++i )
@@ -29,6 +38,18 @@ void TestBuffer( char *p_buff,
     printf("sum: %ld size: %zu\n", sum, n );
 }
 
+void TestBufferInt( int *p_buff,
+                    size_t n )
+{
+    long int sum = 0;
+
+    for ( unsigned int i = 0; i < n; ++i )
+    {
+        sum += p_buff[i];
+    }
+
+    printf("sum: %ld size ( int ): %zu\n", sum, n );
+}
 
 bool ExpandBufferZeroFillExpanded( char **p_buff,
                                    size_t *p_current_size,
@@ -55,6 +76,32 @@ bool ExpandBufferZeroFillExpanded( char **p_buff,
     return true;
 }
 
+bool ExpandBufferZeroFillExpandedInt( int **p_buff,
+                                      size_t *p_current_size,
+                                      size_t chunk_size_to_add )
+{
+
+    size_t new_size = *p_current_size + chunk_size_to_add;
+
+    int *p_tmp = realloc( *p_buff, new_size * sizeof *p_tmp );
+
+    if ( !p_tmp )
+    {
+        printf("ERROR: cannot reallocate memory\n");
+        return false;
+    }
+
+    // Fill the newly allocated memory with zeroes
+    memset( p_tmp + *p_current_size, 0, chunk_size_to_add * sizeof *p_tmp );
+
+    *p_buff = p_tmp;
+
+    *p_current_size = new_size;
+
+    return true;
+}
+
+
 int main()
 {
 
@@ -62,7 +109,7 @@ int main()
 
     printf("(1)\n");
 
-    char *buffer = calloc( current_size, sizeof(char) );
+    char *buffer = calloc( current_size, sizeof *buffer );
 
     if ( !buffer )
     {
@@ -109,6 +156,63 @@ int main()
     TestBuffer( buffer, current_size );
 
     free( buffer );
+    current_size = 0;
+
+    /************************************************************/
+
+    size_t current_size_i = 1024;
+
+    printf("(4)\n");
+
+    int *buffer_i = calloc( current_size_i, sizeof *buffer_i );
+
+    if ( !buffer_i )
+    {
+        printf("ERROR: cannot allocate memory\n");
+        exit(EXIT_FAILURE);
+    }
+
+    TestBufferInt( buffer_i, current_size_i );
+
+    /************************************************************/
+
+    printf("(5)\n");
+
+    FillBufferWithOnesInt( buffer_i, current_size_i );
+    TestBufferInt( buffer_i, current_size_i );
+
+    if ( !ExpandBufferZeroFillExpandedInt( &buffer_i,
+                                           &current_size_i,
+                                           1024 ) )
+    {
+        printf("ERROR: ExpandBufferZeroFillExpandedInt\n");
+        free( buffer_i );
+        exit(EXIT_FAILURE);
+    }
+
+    TestBufferInt( buffer_i, current_size_i );
+
+    /************************************************************/
+
+    printf("(6)\n");
+
+    FillBufferWithOnesInt( buffer_i, current_size_i );
+    TestBufferInt( buffer_i, current_size_i );
+
+    if ( !ExpandBufferZeroFillExpandedInt( &buffer_i,
+                                           &current_size_i,
+                                           1024 ) )
+    {
+        printf("ERROR: ExpandBufferZeroFillExpandedInt\n");
+        free( buffer_i );
+        exit(EXIT_FAILURE);
+    }
+
+    TestBufferInt( buffer_i, current_size_i );
+
+    free( buffer_i );
+
+    /************************************************************/
 
     exit(EXIT_SUCCESS);
 }
