44f2d6cde2c030945755922e37274cf551af9c84
RicardasSim <universe.nomad@gmail.com>
2024-07-10 02:02:56 +0300

    edit

--- pointers/double-arr-arg/main.c
+++ pointers/double-arr-arg/main.c
@@ -5,22 +5,37 @@
 #define NUM 3
 #define LEN 64
 
-void TestFnc( char (*arr)[LEN] )
+void TestFnc1( char (*arr)[LEN] )
 {
     strcpy( arr[0], "first");
     strcpy( arr[1], "second");
     strcpy( arr[2], "third");
 }
 
+void TestFnc2( char arr[][LEN] )
+{
+    strcpy( arr[0], "one");
+    strcpy( arr[1], "two");
+    strcpy( arr[2], "three");
+}
+
 int main()
 {
     char d_arr[NUM][LEN] = { 0 };
 
-    TestFnc( d_arr );
+    TestFnc1( d_arr );
 
     puts( d_arr[0] );
     puts( d_arr[1] );
     puts( d_arr[2] );
+    putchar('\n');
+
+    TestFnc2( d_arr );
+
+    puts( d_arr[0] );
+    puts( d_arr[1] );
+    puts( d_arr[2] );
+    putchar('\n');
 
     return 0;
 }
