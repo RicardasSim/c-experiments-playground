ec7f28c3d2aab5b9872e3396f498578861f51b92
RicardasSim <universe.nomad@gmail.com>
2024-07-12 23:15:17 +0300

    added folder sizeof-various

---
+++ sizeof-various/main.c
@@ -0,0 +1,155 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct st
+{
+    char c;
+    int i;
+    float f;
+    double d;
+    char *p_c;
+    int *p_i;
+    float *p_f;
+    double *p_d;
+    void *p_v;
+} St;
+
+int main()
+{
+
+    printf("sizeof(char): %zu\n", sizeof( char ) );
+    printf("sizeof(int): %zu\n", sizeof( int ) );
+    printf("sizeof(float): %zu\n", sizeof( float ) );
+    printf("sizeof(double): %zu\n", sizeof( double ) );
+
+    {
+
+        char c;
+        int i;
+        float f;
+        double d;
+
+        printf("sizeof c: %zu\n", sizeof c );
+        printf("sizeof i: %zu\n", sizeof i );
+        printf("sizeof f: %zu\n", sizeof f );
+        printf("sizeof d: %zu\n", sizeof d );
+
+        printf("sizeof &c: %zu\n", sizeof &c );
+        printf("sizeof &i: %zu\n", sizeof &i );
+        printf("sizeof &f: %zu\n", sizeof &f );
+        printf("sizeof &d: %zu\n", sizeof &d );
+
+    }
+
+    {
+
+        char *p_c;
+        int *p_i;
+        float *p_f;
+        double *p_d;
+
+        printf("sizeof *p_c: %zu\n", sizeof *p_c );
+        printf("sizeof *p_i: %zu\n", sizeof *p_i );
+        printf("sizeof *p_f: %zu\n", sizeof *p_f );
+        printf("sizeof *p_&d: %zu\n", sizeof *p_d );
+
+        printf("sizeof p_c: %zu\n", sizeof p_c );
+        printf("sizeof p_i: %zu\n", sizeof p_i );
+        printf("sizeof p_f: %zu\n", sizeof p_f );
+        printf("sizeof p_d: %zu\n", sizeof p_d );
+
+    }
+
+    {
+
+        char **p_d_c;
+        int **p_d_i;
+        float **p_d_f;
+        double **p_d_d;
+
+        printf("sizeof **p_d_c: %zu\n", sizeof **p_d_c );
+        printf("sizeof **p_d_i: %zu\n", sizeof **p_d_i );
+        printf("sizeof **p_d_f: %zu\n", sizeof **p_d_f );
+        printf("sizeof **p_d_&d: %zu\n", sizeof **p_d_d );
+
+        printf("sizeof *p_d_c: %zu\n", sizeof *p_d_c );
+        printf("sizeof *p_d_i: %zu\n", sizeof *p_d_i );
+        printf("sizeof *p_d_f: %zu\n", sizeof *p_d_f );
+        printf("sizeof *p_d_&d: %zu\n", sizeof *p_d_d );
+
+        printf("sizeof p_d_c: %zu\n", sizeof p_d_c );
+        printf("sizeof p_d_i: %zu\n", sizeof p_d_i );
+        printf("sizeof p_d_f: %zu\n", sizeof p_d_f );
+        printf("sizeof p_d_d: %zu\n", sizeof p_d_d );
+
+    }
+
+    {
+
+        St s;
+
+        printf("sizeof s: %zu\n", sizeof s );
+        printf("sizeof &s: %zu\n", sizeof &s );
+        printf("sizeof s.c: %zu\n", sizeof s.c );
+        printf("sizeof s.i: %zu\n", sizeof s.i );
+        printf("sizeof s.f: %zu\n", sizeof s.f );
+        printf("sizeof s.d: %zu\n", sizeof s.d );
+        printf("sizeof s.p_c: %zu\n", sizeof s.p_c );
+        printf("sizeof s.p_i: %zu\n", sizeof s.p_i );
+        printf("sizeof s.p_f: %zu\n", sizeof s.p_f );
+        printf("sizeof s.p_d: %zu\n", sizeof s.p_d );
+        printf("sizeof s.p_v: %zu\n", sizeof s.p_v );
+        printf("sizeof *s.p_c: %zu\n", sizeof *s.p_c );
+        printf("sizeof *s.p_i: %zu\n", sizeof *s.p_i );
+        printf("sizeof *s.p_f: %zu\n", sizeof *s.p_f );
+        printf("sizeof *s.p_d: %zu\n", sizeof *s.p_d );
+
+    }
+
+    {
+
+        St *p_s;
+
+        printf("sizeof *p_s: %zu\n", sizeof *p_s );
+        printf("sizeof p_s: %zu\n", sizeof p_s );
+        printf("sizeof p_s->c: %zu\n", sizeof p_s->c );
+        printf("sizeof p_s->i: %zu\n", sizeof p_s->i );
+        printf("sizeof p_s->f: %zu\n", sizeof p_s->f );
+        printf("sizeof p_s->d: %zu\n", sizeof p_s->d );
+        printf("sizeof p_s->p_c: %zu\n", sizeof p_s->p_c );
+        printf("sizeof p_s->p_i: %zu\n", sizeof p_s->p_i );
+        printf("sizeof p_s->p_f: %zu\n", sizeof p_s->p_f );
+        printf("sizeof p_s->p_d: %zu\n", sizeof p_s->p_d );
+        printf("sizeof p_s->p_v: %zu\n", sizeof p_s->p_v );
+        printf("sizeof *p_s->p_c: %zu\n", sizeof *p_s->p_c );
+        printf("sizeof *p_s->p_i: %zu\n", sizeof *p_s->p_i );
+        printf("sizeof *p_s->p_f: %zu\n", sizeof *p_s->p_f );
+        printf("sizeof *p_s->p_d: %zu\n", sizeof *p_s->p_d );
+
+    }
+
+    {
+
+        St **p_d_s;
+
+        printf("sizeof **p_d_s: %zu\n", sizeof **p_d_s );
+        printf("sizeof *p_d_s: %zu\n", sizeof *p_d_s );
+        printf("sizeof p_d_s: %zu\n", sizeof p_d_s );
+        printf("sizeof (*p_d_s)->c: %zu\n", sizeof (*p_d_s)->c );
+        printf("sizeof (*p_d_s)->i: %zu\n", sizeof (*p_d_s)->i );
+        printf("sizeof (*p_d_s)->f: %zu\n", sizeof (*p_d_s)->f );
+        printf("sizeof (*p_d_s)->d: %zu\n", sizeof (*p_d_s)->d );
+        printf("sizeof (*p_d_s)->p_c: %zu\n", sizeof (*p_d_s)->p_c );
+        printf("sizeof (*p_d_s)->p_i: %zu\n", sizeof (*p_d_s)->p_i );
+        printf("sizeof (*p_d_s)->p_f: %zu\n", sizeof (*p_d_s)->p_f );
+        printf("sizeof (*p_d_s)->p_d: %zu\n", sizeof (*p_d_s)->p_d );
+        printf("sizeof (*p_d_s)->p_v: %zu\n", sizeof (*p_d_s)->p_v );
+        printf("sizeof *(*p_d_s)->p_c: %zu\n", sizeof *(*p_d_s)->p_c );
+        printf("sizeof *(*p_d_s)->p_i: %zu\n", sizeof *(*p_d_s)->p_i );
+        printf("sizeof *(*p_d_s)->p_f: %zu\n", sizeof *(*p_d_s)->p_f );
+        printf("sizeof *(*p_d_s)->p_d: %zu\n", sizeof *(*p_d_s)->p_d );
+
+    }
+
+    return 0;
+}
