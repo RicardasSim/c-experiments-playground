fd5e12b2cc801ee37b0611be9efafcbbfbfee646
RicardasSim <universe.nomad@gmail.com>
2024-08-27 08:44:23 +0300

    edit

--- alloc-array-of-struct/main.c
+++ alloc-array-of-struct/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #define MUM_OF_ST 10
 
@@ -11,12 +12,34 @@ typedef struct
     double d;
 } St;
 
+typedef struct
+{
+    char *p_1;
+    char *p_2;
+} St_p;
+
+void Cleanup( St_p *p, unsigned int num )
+{
+    for ( unsigned int i = 0; i < num; ++i )
+    {
+        free( p[i].p_1 );
+        free( p[i].p_2 );
+    }
+
+    free( p );
+}
 
 int main()
 {
 
 
-    St *s_arr = calloc( MUM_OF_ST, sizeof *s_arr );
+    St *s_arr = malloc( MUM_OF_ST * sizeof *s_arr );
+
+    if ( !s_arr )
+    {
+        printf("Error: cannot allocate memory for s_arr.\n");
+        exit(EXIT_FAILURE);
+    }
 
     printf("sizeof St: %zu\n", sizeof( St ) );
     printf("sizeof *s_arr: %zu\n", sizeof *s_arr );
@@ -38,5 +61,85 @@ int main()
 
     free( s_arr );
 
-    return 0;
+    /********************************/
+
+    char str_1[] = "Galaxy";
+    char str_2[] = "Milky Way";
+    char str_3[] = "Constellation";
+    char str_4[] = "Cassiopeia";
+
+    St_p *s_arr_2 = calloc( MUM_OF_ST, sizeof *s_arr_2 );
+
+    if ( !s_arr_2 )
+    {
+        printf("Error: cannot allocate memory for s_arr_2.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    printf("sizeof St_p: %zu\n", sizeof( St_p ) );
+    printf("sizeof *s_arr_2: %zu\n", sizeof *s_arr_2 );
+    printf("sizeof s_arr_2[0]: %zu\n", sizeof s_arr_2[0] );
+    printf("sizeof s_arr_2: %zu\n", sizeof s_arr_2 );
+    printf("sizeof *str_1: %zu\n", sizeof *str_1 );
+
+    /***********/
+
+    s_arr_2[0].p_1 = malloc( ( strlen( str_1 ) + 1 ) * sizeof *str_1 );
+
+    if ( !s_arr_2[0].p_1 )
+    {
+        printf("Error: cannot allocate memory (1).\n");
+        Cleanup( s_arr_2, MUM_OF_ST );
+        exit(EXIT_FAILURE);
+    }
+
+    strcpy( s_arr_2[0].p_1, str_1 );
+
+    /***********/
+
+    s_arr_2[0].p_2 = malloc( ( strlen( str_2 ) + 1 ) * sizeof *str_2 );
+
+    if ( !s_arr_2[0].p_2 )
+    {
+        printf("Error: cannot allocate memory (2).\n");
+        Cleanup( s_arr_2, MUM_OF_ST );
+        exit(EXIT_FAILURE);
+    }
+
+    strcpy( s_arr_2[0].p_2, str_2 );
+
+    /***********/
+
+    s_arr_2[1].p_1 = malloc( ( strlen( str_3 ) + 1 ) * sizeof *str_3 );
+
+    if ( !s_arr_2[1].p_1 )
+    {
+        printf("Error: cannot allocate memory (3).\n");
+        Cleanup( s_arr_2, MUM_OF_ST );
+        exit(EXIT_FAILURE);
+    }
+
+    strcpy( s_arr_2[1].p_1, str_3 );
+
+    /***********/
+
+    s_arr_2[1].p_2 = malloc( ( strlen( str_4 ) + 1 ) * sizeof *str_4 );
+
+    if ( !s_arr_2[1].p_2 )
+    {
+        printf("Error: cannot allocate memory (4).\n");
+        Cleanup( s_arr_2, MUM_OF_ST );
+        exit(EXIT_FAILURE);
+    }
+
+    strcpy( s_arr_2[1].p_2, str_4 );
+
+    /***********/
+
+    printf("%s %s\n", s_arr_2[0].p_1, s_arr_2[0].p_2 );
+    printf("%s %s\n", s_arr_2[1].p_1, s_arr_2[1].p_2 );
+
+    Cleanup( s_arr_2, MUM_OF_ST );
+
+    exit(EXIT_SUCCESS);
 }
